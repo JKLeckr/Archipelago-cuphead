dcb0f14ca68363638f057c5447412f650ff02094
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2025-06-01 11:04:55 -0500

    Updated debug to not spam on tests unless specified

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 from typing import Union # type: ignore
+import os
 import settings
 
 class CupheadSettings(settings.Group):
@@ -9,4 +10,6 @@ class CupheadSettings(settings.Group):
     debug: Union[Debug, int] = 0 # type: ignore
 
     def is_debug_bit_on(self, bit: int):
+        if "PYTEST_CURRENT_TEST" in os.environ and (int(self.debug) & 512) == 0:
+            return False
         return (int(self.debug) & bit) == bit
