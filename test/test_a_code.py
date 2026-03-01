0694a393990532110ee29e8cfe7ad5b62248c644
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2026-03-01 15:10:31 -0600

    Removed old rules code and data

--- test/test_a_code.py
+++ test/test_a_code.py
@@ -5,13 +5,8 @@ import unittest
 
 from ..world.levels import levelids
 from ..world.options.optionbase import _levelset
-from ..world.rules.levelrules.legacy.levelruledata import LevelRuleData
 
 
 class TestCode(unittest.TestCase):
     def test_level_set(self):
         self.assertEqual(_levelset.levels, set(levelids.level_ids.values()))
-
-    def test_levelrule_data_parser(self):
-        data = LevelRuleData.get_data()
-        self.assertTrue(data)
