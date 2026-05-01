73a54afa865cd453dbefb58a3d511ffb22d75c55
JKLeckr <11635283+JKLeckr@users.noreply.github.com>
2026-04-30 20:56:10 -0500

    Cleaned up shop mode switch

--- world/options/constantoptions.py
+++ world/options/constantoptions.py
@@ -87,7 +87,6 @@ class RandomizeAimAbilities(BoolOption, ConstToggle, NamedOption):
     value = 0
 
 
-# TODO: Implement
 class ShopMode(EnumOption[e.ShopMode], ConstNumericOption, NamedOption):
     """
     --NOT SUPPORTED OPTION--
@@ -100,8 +99,7 @@ class ShopMode(EnumOption[e.ShopMode], ConstNumericOption, NamedOption):
     enum_type = e.ShopMode
     #visibility = Visibility.spoiler
     option_tiers = 0
-    #option_strict_tiers = 1
-    option_independent = 2
+    option_independent = 1
     default = 0
     value = 0
 
